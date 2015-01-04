03963710ccf058119be17e474d703c848b231d2b
QQ1350995917 <1350995917@qq.com>
2015-01-04 14:02:34 +0800

    Update macpro.sh

--- mac/macpro.sh
+++ mac/macpro.sh
@@ -1,3 +1,9 @@
+mkdir bin
+cd bin/
+mkdir homebrew
+cd homebrew/
+curl -L http://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C /usr/local/homebrew/
+
 brew update
 brew install java
 brew install git
