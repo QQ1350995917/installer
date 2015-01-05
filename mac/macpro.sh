84678a8a37dcf93e90f5958622e14329a82d816c
QQ1350995917 <1350995917@qq.com>
2015-01-05 13:35:18 +0800

    Update macpro.sh
    homebrew

--- mac/macpro.sh
+++ mac/macpro.sh
@@ -2,10 +2,14 @@ sudo passwd root
 echo "alias vi='vim'">> /etc/profile
 echo "alias ll='ls -l'">> /etc/profile
 source /etc/profile 
+
 mkdir bin
 cd bin/
 mkdir homebrew
 cd homebrew/
+#方式1：
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#方式2：
 curl -L http://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C /usr/local/homebrew/
 
 brew update
